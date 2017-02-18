e370f8e720a104f846bcb4e9fbdfc48cda5ea2c4
Ricky <rrvillareal@users.noreply.github.com>
2017-02-18 15:28:01 -0800

    Update getdata.c

--- getdata.c
+++ getdata.c
@@ -1,14 +1,3 @@
-/***
-Robert Villareal
-rrvillareal@csu.fullerton.edu
-CPSC223C-01
-Assignment 1
-Due: 2/11/2017
-Program title: Frequency counting
-File name: assignment1_getdata.c
-Purpose: This getdata function gets data from user.
-
-***/
 #include <stdio.h>
 
 int getdata(int values[], int arraysize)
