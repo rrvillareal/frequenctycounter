054bf8e8eac81847a5e00baecbf3ce2ae2110fed
Ricky <rrvillareal@users.noreply.github.com>
2017-02-18 15:28:24 -0800

    Update showfrequencytable.c

--- showfrequencytable.c
+++ showfrequencytable.c
@@ -1,14 +1,3 @@
-/***
-Robert Villareal
-rrvillareal@csu.fullerton.edu
-CPSC223C-01
-Assignment 1
-Due: 2/11/2017
-Program title: Frequency counting
-File name: assignment1_showfrequencytable.c
-Purpose: This main function shows the frequency table that was created.
-***/
-
 #include <stdio.h>
 
 void showfrequencytable(int frequencies[], int largestdatavalue)
