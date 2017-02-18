556f18102e3905a514ca6822e2e1a74e5c043a68
Ricky <rrvillareal@users.noreply.github.com>
2017-02-18 15:28:13 -0800

    Update makefrequencytable.c

--- makefrequencytable.c
+++ makefrequencytable.c
@@ -1,14 +1,3 @@
-/***
-Robert Villareal
-rrvillareal@csu.fullerton.edu
-CPSC223C-01
-Assignment 1
-Due: 2/11/2017
-Program title: Frequency counting
-File name: assignment1_makefrequencytable.c
-Purpose: This main function makes the frequency table.
-***/
-
 #include <stdio.h>
 
 void makefrequencytable(int values[ ], int actualsizeofcollection, int frequencies[ ], int largestdatavalue)
